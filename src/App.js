83306f37d4032af1b30b281e2555b86877891d16
divyanshkhetan <divyanshkhetan18@gmail.com>
2021-10-10 13:40:38 +0530

    fixed the dp=ouble rendering of homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
 import Login from './components/pages/Login';
+import Signup from './components/pages/Signup';
 
 function App() {
   return (
     <Router>
       <Route exact path='/' component={Home} />
       <Route exact path='/login' component={Login} />
-      <Route exact path='/' component={Home} />
+      <Route exact path='/signup' component={Signup} />
     </Router>
   );
 }
