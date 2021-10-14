06c8f99128a8a4ceff1e755eccfcbcf87e351084
divyanshkhetan <divyanshkhetan18@gmail.com>
2021-10-14 17:00:04 +0530

    added database connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 const PORT = 3001;
+
+// Creating connection
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'cms'
+});
+
+db.connect( err => {
+    if(err) {
+        throw err;
+    } 
+    console.log("MySql Connected as id: " + db.threadId);
+});
+
 
 app.use(express.json());
 
